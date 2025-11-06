b2e0dd2ebd829a60df9b180770708afa31d2cf19
Henry Odza <hwodza@gmail.com>
2025-11-06 15:09:40 -0500

    added flake for simple devenv

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Rust learning repo, basic dev flake";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.rustc pkgs.cargo pkgs.rust-analyzer ];
+      };
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "myapp";
+        version = "0.0.1";
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+    };
+}
